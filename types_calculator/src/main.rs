85166dea3d3719a6dbd5b08b347668d6d3d1d3e3
Brian Fakhoury <brianfakhoury@gmail.com>
2023-01-17 09:49:23 -0500

    basic calculator

---
+++ types_calculator/src/main.rs
@@ -0,0 +1,37 @@
+use text_io::read;
+
+#[derive(Debug)]
+enum Components {
+    Value(i32),
+    Plus,
+}
+
+fn parse_into_components(raw_string: String) -> Vec<Components> {
+    let components: Vec<Components> = raw_string
+        .split_whitespace()
+        .map(|x| match x.parse::<i32>() {
+            Ok(n) => Components::Value(n),
+            Err(_) => Components::Plus,
+        })
+        .collect();
+    components
+}
+
+fn compute_from_components(comp_array: Vec<Components>) -> String {
+    let mut computed: i32 = 0;
+    for component in comp_array {
+        match component {
+            Components::Value(n) => computed = computed + n,
+            Components::Plus => (),
+        }
+    }
+    computed.to_string()
+}
+
+fn main() {
+    let a: String = read!("{}\n");
+    let parsed = parse_into_components(a);
+    println!("Parsed into {:?}", parsed);
+    let answer = compute_from_components(parsed);
+    println!("Final answer: {}", answer);
+}
